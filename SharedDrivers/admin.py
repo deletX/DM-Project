8eff55a59ee1bb48ba973be9b1529bf895ea5e9a
stefano <gavioli.stefano.9@gmail.com>
2020-02-28 19:26:44 +0100

    Added admin stuff

--- SharedDrivers/admin.py
+++ SharedDrivers/admin.py
@@ -1,9 +1,30 @@
 from django.contrib import admin
 from .models import Profile, Event, Participant, Car
 
-admin.site.register(Event)
-admin.site.register(Car)
-admin.site.register(Profile)
-admin.site.register(Participant)
+
+class EventAdmin(admin.ModelAdmin):
+    fields = ['name', 'description', 'destination', 'date_time', 'owner']
+    list_filter = ['date_time']
+    search_fields = ['name']
+
+
+class CarAdmin(admin.ModelAdmin):
+    fields = ['name', 'tot_avail_seats', 'consumption', 'fuel']
+    list_filter = ['fuel', 'tot_avail_seats']
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    fields = ['score']
+
+
+class ParticipantAdmin(admin.ModelAdmin):
+    fields = ['car', 'starting_pos', 'event']
+    search_fields = ['event', 'user']
+
+
+admin.site.register(Event, EventAdmin)
+admin.site.register(Car, CarAdmin)
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Participant, ParticipantAdmin)
 
 # Register your models here.
--- SharedDrivers/models.py
+++ SharedDrivers/models.py
@@ -2,6 +2,8 @@ from django.contrib.gis.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from django.core.exceptions import ValidationError
 
 
 class Car(models.Model):
@@ -13,14 +15,22 @@ class Car(models.Model):
 
     name = models.CharField(max_length=50)
     tot_avail_seats = models.IntegerField(validators=[MinValueValidator(2), MaxValueValidator(9)])
-    consumption = models.FloatField(default=10.0)  # l/100km
+    consumption = models.FloatField(default=10.0)
+
+    def __str__(self):
+        return self.name
+    # l/100km
     fuel = models.SmallIntegerField(choices=FuelChoices.choices, default=1)
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     score = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.user.username
 
 
 class Event(models.Model):
@@ -30,6 +40,9 @@ class Event(models.Model):
     date_time = models.DateTimeField(default=timezone.now)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
+
 
 class Participant(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -38,5 +51,12 @@ class Participant(models.Model):
     pickup_index = models.SmallIntegerField(default=-1)
     expense = models.FloatField(default=-1)
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
+
+    def clean_fields(self, exclude=None):
+        if self.car not in self.user.car_set:
+            raise ValidationError(_('Car should be one of the user\'s'))
+
+    def __str__(self):
+        return self.user.username
 
 # Create your models here.
