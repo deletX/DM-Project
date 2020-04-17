1152a2c2bb1b2a210942f779c6694f23441ed825
stefano <gavioli.stefano.9@gmail.com>
2020-04-17 15:28:22 +0200

    Change Proj Structure

---
+++ static/precache-manifest.de9374ef7f90f2218fce0c8acd841062.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89ba271915546d888a008f662ec7fb5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4463ed872a48e8247dc2",
+    "url": "/static/css/main.0bdea483.chunk.css"
+  },
+  {
+    "revision": "1b488ca7c92ada6b9aad",
+    "url": "/static/js/2.2ce241ce.chunk.js"
+  },
+  {
+    "revision": "0cc250496b44f02fdf890e18de9e41b4",
+    "url": "/static/js/2.2ce241ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4463ed872a48e8247dc2",
+    "url": "/static/js/main.8060c67b.chunk.js"
+  },
+  {
+    "revision": "f94c826f23d6125c1497",
+    "url": "/static/js/runtime-main.1434e7d1.js"
+  },
+  {
+    "revision": "fb42d919e02fd6ec7dd62fca3f0b57e2",
+    "url": "/static/media/GoogleLogo.fb42d919.svg"
+  },
+  {
+    "revision": "3ea4a20206228fcb1cb0ea3e67b4e319",
+    "url": "/static/media/logo.3ea4a202.svg"
+  }
+]);
