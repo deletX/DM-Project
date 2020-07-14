e1875d015eb0c1293a8944084124d85141955651
stefano <gavioli.stefano.9@gmail.com>
2020-07-14 10:44:12 +0200

    corrected for dev

--- react-fontend/src/constants/constants.js
+++ react-fontend/src/constants/constants.js
@@ -5,7 +5,7 @@ export const CLIENT_SECRET = "KsSTbOaPDbxEbEGqkcyWl3-v";
 export const APP_CLIENTID = "4mOLHPfJL0zueHOlawvJXsdnImpjOv3PmLdmm3NT";
 export const APP_SECRET = "FW4dYuZsLmE6PQHk7qrPAc4shQhdx2hknqNOh58XO3JQ6PFI8um2z6wyJubxF6hKNOOOJfZUQ67jm5ApN5HJioq4XsNAGSbCLiQZqrqfo6jiy67ddpvMOl3Be8SATFSL";
 
-export const serverURL = "/";
+export const serverURL = "localhost:8000/";
 
 export const status = [
     'Joinable',
