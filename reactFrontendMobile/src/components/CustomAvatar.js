6e6f3902538b424cd058e6afac4a76f6de499131
stefano <gavioli.stefano.9@gmail.com>
2020-07-28 11:36:39 +0200

    CustomAvatar enhancing

--- reactFrontendMobile/src/components/CustomAvatar.js
+++ reactFrontendMobile/src/components/CustomAvatar.js
@@ -9,10 +9,11 @@ const CustomAvatar = (props) => {
         (props.picture === null || props.picture === "") ? (
             <Avatar.Text
                 label={`${props.firstName ? props.firstName[0] : ""}${props.lastName ? props.lastName[0] : ""}`.toUpperCase()}
-                size={50}
-                labelStyle={{fontSize: 21, color: Colors.white}}/>
+                size={props.size ? props.size : 50}
+                labelStyle={[{fontSize: 21, color: Colors.white}, props.labelStyle]}/>
         ) : (
-            <Avatar.Image source={{uri: props.picture === '' ? null : props.picture}} size={50}/>
+            <Avatar.Image source={{uri: props.picture === '' ? null : props.picture}}
+                          size={props.size ? props.size : 50}/>
 
         )
     );
