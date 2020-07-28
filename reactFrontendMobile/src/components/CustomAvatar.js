1910814b4e82e29f4d80eaba660067e699385df5
stefano <gavioli.stefano.9@gmail.com>
2020-07-28 10:54:52 +0200

    fixed for undefined firstName or lastName

--- reactFrontendMobile/src/components/CustomAvatar.js
+++ reactFrontendMobile/src/components/CustomAvatar.js
@@ -7,8 +7,10 @@ const CustomAvatar = (props) => {
     if (props.firstName !== undefined) console.log(`, firstName[0]lastName[0]Upp: ${`${props.firstName[0]}${props.lastName[0]}`.toUpperCase()}\`)`)
     return (
         (props.picture === null || props.picture === "") ? (
-            <Avatar.Text label={`${props.firstName[0]}${props.lastName[0]}`.toUpperCase()} size={50}
-                         labelStyle={{fontSize: 21, color: Colors.white}}/>
+            <Avatar.Text
+                label={`${props.firstName ? props.firstName[0] : ""}${props.lastName ? props.lastName[0] : ""}`.toUpperCase()}
+                size={50}
+                labelStyle={{fontSize: 21, color: Colors.white}}/>
         ) : (
             <Avatar.Image source={{uri: props.picture === '' ? null : props.picture}} size={50}/>
 
