d46bbe3f657dc5638b7f40754850bc41f509a4b1
stefano <gavioli.stefano.9@gmail.com>
2020-09-02 15:08:55 +0200

    JoinComponent.jsx refactor

--- react-fontend/src/components/event/JoinComponent.jsx
+++ react-fontend/src/components/event/JoinComponent.jsx
@@ -5,34 +5,16 @@ import Select from "@material-ui/core/Select";
 import MapContainer from "../../containers/MapContainer";
 import {makeStyles} from "@material-ui/core/styles";
 import MenuItem from "@material-ui/core/MenuItem";
-import uuid from "node-uuid";
 import {connect} from "react-redux";
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        marginTop: theme.spacing(2),
-        marginBottom: theme.spacing(2),
-        width: '90%',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-    },
-    form: {
-        width: '100%',
-    }
-}))
-
-const JoinComponent = ({
-                           cars, addr, setAddr, pos, setPos, car, setCar
-                       }) => {
+const JoinComponent = ({cars, addr, setAddr, pos, setPos, car, setCar}) => {
     const classes = useStyles();
-
 
     let carMenuItems = cars.map(car => (
         <MenuItem key={car.id} value={car.id}>{car.name}, {car.tot_avail_seats} seats</MenuItem>
     ))
+    carMenuItems.push(<MenuItem key={-1} value={-1}>No car</MenuItem>)
 
-    carMenuItems.push(<MenuItem key={uuid()} value={-1}>No car</MenuItem>)
     return (
         <div className={classes.root}>
             <FormControl variant={"outlined"} className={classes.form}>
@@ -50,6 +32,19 @@ const JoinComponent = ({
     );
 };
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        marginTop: theme.spacing(2),
+        marginBottom: theme.spacing(2),
+        width: '90%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    form: {
+        width: '100%',
+    }
+}))
 
 function mapStateToProps(state) {
     return {
